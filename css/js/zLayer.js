3cd067f125aa271ebe1f0efab978e48ef32c6fb2
liuwz <liuwz@tiens.com>
2019-09-10 09:06:57 +0800

    去掉冗余优化代码，优化帮助信息

--- css/js/zLayer.js
+++ css/js/zLayer.js
@@ -1,12 +1,27 @@
 // 点击元素Z轴浮动效果插件
+// 应用于移动端，可以使内容在XY轴的二维层面增加Z轴的三维展示
+// 基本构造格式:
+// =======================================================
+// <ul id="应用插件的ID">
+// 	<li>
+// 		<h3 class="text_title">标题</h3>
+// 		<img src="标题图片" class="text_img">
+// 		<div class="text_content">内容</div>
+// 	</li>
+// 	......
+// </ul>
+// var 自定义名称 = new zLayers('应用插件的ID');
+// =======================================================
 // 作者：liuwz
-// 版本号：V1.0 日期：2019.09.06
+// Git：https://github.com/rocker2010/zLayer
+// 版本号：V1.0
 
 
 function zLayers(selector) {
 	/* 变量改为属性 */
 	this.zLayer = document.querySelector(selector);
 	this.zLayerList = this.zLayer.querySelectorAll('li');
+	this.zLayerImg = this.zLayer.querySelectorAll('img');
 	this.zLayerWidth = this.zLayer.querySelector('li').offsetWidth;
 	this.zLayerHeight = this.zLayer.querySelector('li').offsetHeight;
 	this.zLayerListLength = this.zLayerList.length;
@@ -16,30 +31,37 @@ function zLayers(selector) {
 
 zLayers.prototype = {
 	constructor: zLayers,
-
 	/* 功能抽离，此处实现初始化 */
 	init: function() {
+		// 此处去掉列表图片在移动端的点击预览
+		for (var i = 0; i < this.zLayerImg.length; i++) {
+			this.zLayerImg[i].addEventListener('click', function(e) {
+				e.preventDefault();
+			});
+		}
+
 		var Layerwidth = this.zLayerWidth
 		LayerHeight = this.zLayerHeight
 		ListLength = this.zLayerListLength
 		LayerList = this.zLayerList
 
+		// 点击显示详细函数
 		function showLayer(that) {
 			that.classList.add("zLayerShow");
-			that.style.cssText = "position:fixed;z-index:999999;width:" + Layerwidth +"px;box-shadow: 8px 16px 32px #3c3c3c;"
+			that.style.cssText = "position:fixed;z-index:999999;width:" + Layerwidth + "px;box-shadow: 8px 16px 32px #3c3c3c;"
 			var showBefHeight = that.offsetHeight;
 			that.getElementsByClassName("text_title")[0].append(closebtn);
 			var showHeight = that.offsetHeight;
-			that.style.height=LayerHeight+"px";
+			that.style.height = LayerHeight + "px";
 			var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
 			var layerTop = document.querySelector(".zLayerPlace").offsetTop - scrollTop;
 			that.style.cssText = that.style.cssText + "top:" + layerTop + "px;"
-			// console.log(document.documentElement.scrollTop + ":" + document.querySelector(".zLayerPlace").offsetTop);
 			setTimeout(function() {
 				that.style.cssText = that.style.cssText + "top:50%;transform:translateY(-50%);height:" + showHeight + "px"
 			}, 400)
 		};
 
+		// 点击关闭按钮关闭显示
 		var closebtn = document.createElement("span");
 		closebtn.innerHTML = "×";
 		closebtn.className = 'closeBtn';
@@ -91,6 +113,8 @@ zLayers.prototype = {
 				window.event.cancelBubble = true;
 			}
 		};
+
+		// 给列表增加点击显示的功能
 		for (var i = 0; i < this.zLayerListLength; i++) {
 			this.zLayerList[i].onclick = function() {
 				var that = this;
